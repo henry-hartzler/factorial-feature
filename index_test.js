83e832aceec3e7a61407e24b49c4a3ea7dcbbb47
Henry Hartzler <hartzler.henry@gmail.com>
2022-05-26 19:24:00 -0600

    first commit

---
+++ index_test.js
@@ -0,0 +1,35 @@
+var assert = require("assert");
+var Calculate =  require('../index.js')
+
+describe('Calculate', () => {
+  describe('.factorial', () => {
+    it('will test if 5! is equal to 120', () => {
+      //setup
+      const expectedResult = 120;
+      const input = 5;
+      //exercise
+      const result = Calculate.factorial(input);
+      //verify
+      assert.equal(result, expectedResult);
+    });
+    
+    it('will test if 3! is equal to 6', () => {
+      //setup
+      const expectedResult = 6;
+      const input = 3;
+      //exerise
+      const result = Calculate.factorial(input);
+      //verify
+      assert.equal(result, expectedResult);
+    });
+    it('will block an error message that covers the 0! edge case', () => {
+      //setup
+      const input = 0;
+      const expectedResult = 1;
+      //exercise
+      const result = Calculate.factorial(input);
+      //verify
+      assert.equal(result, expectedResult);
+    });
+  });
+});
